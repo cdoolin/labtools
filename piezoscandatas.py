2eb3faa4aeb9eb7957d676dc175371850aceedb7
callum <doolin@ualberta.ca>
2014-02-24 17:56:45 -0700

    added megadaq saving and changed to specify sa location

--- piezoscandatas.py
+++ piezoscandatas.py
@@ -21,22 +21,49 @@ pars.add_argument("-l", "--lower", type=float, default=1., help="set lower volt"
 pars.add_argument("-u", "--upper", type=float, default=100., help="set upper volt")
 pars.add_argument("-r", "--reverse", action='store_true', help="scan in reverse")
 pars.add_argument("-P", "--noplot", action='store_true', help="disable plotting at end of scan")
-pars.add_argument("-s", "--sa", action='store_true', help="tell SA to save at each volt")
+pars.add_argument("-s", "--sa", action='store', help="specify SA server to save with")
+pars.add_argument("-m", "--megadaq", action='store', help="specify megadaq server to save with")
 pars.add_argument("-W", "--nowavelength", default=False, action='store_true', help="don't use wavelength meter")
 pars.add_argument("-S", "--nosave", action='store_true', help="disable saving")
 pars.add_argument("-o", "--outfile", default="", help="save data to OUTFILE")
 pars.add_argument("--channel", default="Dev1/ai2", help="set nidaq channel to read")
 pars.add_argument("--laser", default="localhost", help="set lasernet address")
 
-
 args = pars.parse_args()
 
+
+#
+# megadaq driver
+#
+if args.megadaq is not None:
+    print("saving megadata at %s" % args.megadaqtyl)
+    import zmq
+    
+    context = zmq.Context()
+    s = context.socket(zmq.REQ)
+    s.connect("tcp://%s:6497" % args.megadaq)
+    
+    def megasave(desc):
+        s.send("acquire;%s" % desc)
+        s.recv()
+else:
+    def megasave(d):
+        pass
+# end megadaq driver
+
+
+#
+# SA
+#
 if not args.sa:
     # hackity hack hack
     labdrivers.SA = labdrivers.SAMock
 
+s = labdrivers.SA()
+s.endsave()
+# end sa 
 
-# initialize drivers
+# otehr drivers
 # lasernet client (through websockets)
 l = labdrivers.LaserClient(args.laser)
 # wl meter
@@ -46,10 +73,11 @@ if not w.ok():
 # nidaq
 d = labdrivers.SimpleDaq(args.channel, 10000, 100, minv=0., maxv=10.)
 # SA
-s = labdrivers.SA()
-if not w.ok():
-    print("couldn't connect to sa")
-s.endsave()
+
+
+#
+# setup scan parameters
+#
 
 # piezo volts to take measurements at
 pz = arange(args.lower, args.upper, args.step).round(1)
@@ -69,22 +97,28 @@ avgs = zeros_like(pz)
 stds = zeros_like(pz)
 wlrs = zeros_like(pz)
 
-print("scanning..." )
+print("scanning...")
 
+#
+# scan!
 # loop through volts and measure at each one
+#
 for i in range(len(pz)):
     l.set_volt(pz[i])
     sleep(.05)
     
     trans1 = d.read()
-    trans2 = d.read()
 
+    #save sa and or megadaq
     s.save(1, i)
+    megasave(i)
 
     if not args.nowavelength and pz[i] in wls:
         wlrs[i] = w.wl()
     else:
         wlrs[i] = 0
+
+    trans2 = d.read()
 
     avgs[i] = mean(r_[trans1, trans2])
     stds[i] = std(r_[trans1, trans2])
