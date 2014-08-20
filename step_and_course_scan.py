31287c483862c96a1a54c1cd50523e0e4848a68e
Callum Doolin <doolin@ualberta.ca>
2014-08-20 13:35:31 -0600

    renamed step_and_scan.py to step_and_fine_scan.py and added course scan alternative

---
+++ step_and_course_scan.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+from numpy import *
+
+#import labdrivers
+from time import sleep, time
+from datetime import datetime
+
+
+# use argparse module to read commandline options
+import argparse
+
+pars = argparse.ArgumentParser(description="""
+steps attocube offset voltages while taking a course scan using lasernet""")
+
+pars.add_argument("off_start", type=float, help="start offset voltage")
+pars.add_argument("off_stop", type=float, help="stop offset voltage")
+pars.add_argument("off_step", type=float, help="offset voltage step")
+
+pars.add_argument("ft_start", type=float, help="start wavelength")
+pars.add_argument("ft_stop", type=float, help="stop wavelength")
+pars.add_argument("ft_step", type=float, help="scan speed")
+
+pars.add_argument("-a", "--axis", type=int, default=1,
+    help="attocube axis to use (default: 1)")
+
+pars.add_argument("-s", "--server", type=str, default="127.0.0.1",
+    help="location of scantech or lasernet server (default: localhost)")
+pars.add_argument("-p", "--port", type=str, default="COM6",
+    help="com port of attocube (default: COM6)")
+
+
+#pars.add_argument("-f", "--fname", type=str, default="finescan")
+
+args = pars.parse_args()
+
+
+# initialize drivers
+from labdrivers.websocks import LaserClient
+laser = LaserClient(server=args.laser)
+
+
+# attocube
+from labdrivers.attocube import Attocube
+atto = Attocube(port=args.port)
+
+
+# fine tunings.  santec laser has set precision of 1 pm (0.01 fine tunings)
+fts = arange(args.ft_start, args.ft_stop, args.ft_step).round(decimals=2)
+# offets
+offs = arange(args.off_start, args.off_stop, args.off_step)
+
+
+avgs = zeros((len(offs), len(fts)))
+stds = zeros_like(avgs)
+
+print("scanning... (control-c to stop)")
+
+for i, offset in enumerate(offs):
+	print("%f V" % offset)
+	laser.set_fine(fts[0])
+	atto.set_offset(args.axis, offset)
+	sleep(.1)
+
+	for j, ft in enumerate(fts):
+		laser.set_fine(ft)
+		sleep(.01)
+
+		trans = daq.read()
+
+		avgs[i, j] = mean(trans)
+		stds[i, j] = std(trans)
+print("finished")
+
+
+timestamp = datetime.now().strftime("%y.%m.%d_%H%M%S_") + args.fname
+print("saving to %s" % timestamp)
+save(timestamp + "_trans", avgs)
+save(timestamp + "_stds", stds)
+save(timestamp + "_offs", offs)
+save(timestamp + "_fts", fts)
