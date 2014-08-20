a3e7031b23fe89f23c27f7e7a5b25fc8b7e88628
Callum Doolin <doolin@ualberta.ca>
2014-08-20 17:13:12 -0600

    init step_and_course_scan.py

--- step_and_course_scan.py
+++ step_and_course_scan.py
@@ -16,9 +16,9 @@ pars.add_argument("off_start", type=float, help="start offset voltage")
 pars.add_argument("off_stop", type=float, help="stop offset voltage")
 pars.add_argument("off_step", type=float, help="offset voltage step")
 
-pars.add_argument("ft_start", type=float, help="start wavelength")
-pars.add_argument("ft_stop", type=float, help="stop wavelength")
-pars.add_argument("ft_step", type=float, help="scan speed")
+pars.add_argument("wl_start", type=float, help="start wavelength")
+pars.add_argument("wl_stop", type=float, help="stop wavelength")
+pars.add_argument("wl_slew", type=float, help="scan speed")
 
 pars.add_argument("-a", "--axis", type=int, default=1,
     help="attocube axis to use (default: 1)")
@@ -33,48 +33,33 @@ pars.add_argument("-p", "--port", type=str, default="COM6",
 
 args = pars.parse_args()
 
-
 # initialize drivers
 from labdrivers.websocks import LaserClient
 laser = LaserClient(server=args.laser)
-
 
 # attocube
 from labdrivers.attocube import Attocube
 atto = Attocube(port=args.port)
 
-
-# fine tunings.  santec laser has set precision of 1 pm (0.01 fine tunings)
-fts = arange(args.ft_start, args.ft_stop, args.ft_step).round(decimals=2)
 # offets
 offs = arange(args.off_start, args.off_stop, args.off_step)
+if any(a - floor(a) > 0):
+    yn = raw_input("volt offsets truncated in filename. continue? [y/N]")
+    if yn != "y":
+        import sys
+        sys.exit(0)
 
-
-avgs = zeros((len(offs), len(fts)))
-stds = zeros_like(avgs)
+# calculate number of digits to use when saving
+digits = ceil(log10(max(abs(offs)) + .001))
 
 print("scanning... (control-c to stop)")
 
 for i, offset in enumerate(offs):
 	print("%f V" % offset)
-	laser.set_fine(fts[0])
+	#laser.set_fine(fts[0])
 	atto.set_offset(args.axis, offset)
 	sleep(.1)
-
-	for j, ft in enumerate(fts):
-		laser.set_fine(ft)
-		sleep(.01)
-
-		trans = daq.read()
-
-		avgs[i, j] = mean(trans)
-		stds[i, j] = std(trans)
+    laser.scan(args.wl_start, args._wl_stop, args._wl_slew, save="%%0%d.0f" % digits % offset)
+	
 print("finished")
 
-
-timestamp = datetime.now().strftime("%y.%m.%d_%H%M%S_") + args.fname
-print("saving to %s" % timestamp)
-save(timestamp + "_trans", avgs)
-save(timestamp + "_stds", stds)
-save(timestamp + "_offs", offs)
-save(timestamp + "_fts", fts)
