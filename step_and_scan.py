5a245f56c469c2d69f79d1df66fd81e6258b9c11
Callum Doolin <doolin@ualberta.ca>
2014-08-20 11:08:05 -0600

    added lasernet piezo mode to step_and_scan.py

--- step_and_scan.py
+++ step_and_scan.py
@@ -25,8 +25,9 @@ pars.add_argument("ft_step", type=float, help="fine tuning step")
 pars.add_argument("-a", "--axis", type=int, default=1,
     help="attocube axis to use (default: 1)")
 
-pars.add_argument("-l", "--laser", type=str, default="127.0.0.1",
-    help="location of scantech server (default: localhost)")
+pars.add_argument("-s", "--server", type=str, default="127.0.0.1",
+    help="location of scantech or lasernet server (default: localhost)")
+pars.add_argument("-t", "--type", type=str, default="scantech", help="either 'scantech' (default) or 'lasernet'")
 pars.add_argument("-c", "--channel", type=str, default="/dev1/ai0",
     help="nidaqmx channel, when using bash on windows escape first / with // (default: /dev1/ai0)")
 pars.add_argument("-p", "--port", type=str, default="COM6",
@@ -41,8 +42,24 @@ args = pars.parse_args()
 
 # initialize drivers
 # scantech client (through websockets)
-from labdrivers.websocks import ScantechClient
-laser = ScantechClient(args.laser)
+if args.type in ['scantech', 'st', 's']:
+    from labdrivers.websocks import ScantechClient
+    laser = ScantechClient(server=args.laser)
+
+    def set_ft(ft):
+        laser.set_fine(ft)
+
+elif args.type in ['lasernet', 'ln', 'l']
+    from labdrivers.websocks import LaserClient
+    laser = LaserClient(server=args.laser)
+
+    def set_ft(ft):
+        laser.set_volt(ft)
+else:
+    print("invalid laser type specified")
+    import sys
+    sys.exit(1)
+
 
 # nidaq
 from labdrivers.daq import SimpleDaq
